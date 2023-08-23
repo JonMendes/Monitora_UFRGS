07692930a1d984a95484fe7d65a17aa4c39836a6
Eric Pisoni <ericperacchi@gmail.com>
2023-08-22 22:28:22 -0300

    Código de exemplo removido

--- src/router.js
+++ src/router.js
@@ -2,17 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
-const tasksController = require('./controllers/tasksController');
-const tasksMiddleware = require('./middlewares/tasksMiddleware');
+// Coisas do exemplo
+// const tasksController = require('./controllers/tasksController');
+// const tasksMiddleware = require('./middlewares/tasksMiddleware');
 
-router.get('/tasks', tasksController.getAll);
-router.post('/tasks', tasksMiddleware.validateFieldTitle, tasksController.createTask);
-router.delete('/tasks/:id', tasksController.deleteTask);
-router.put('/tasks/:id',
-  tasksMiddleware.validateFieldTitle,
-  tasksMiddleware.validateFieldStatus,
-  tasksController.updateTask,
-);
+// router.get('/tasks', tasksController.getAll);
+// router.post('/tasks', tasksMiddleware.validateFieldTitle, tasksController.createTask);
+// router.delete('/tasks/:id', tasksController.deleteTask);
+// router.put('/tasks/:id',
+//   tasksMiddleware.validateFieldTitle,
+//   tasksMiddleware.validateFieldStatus,
+//   tasksController.updateTask,
+// );
 
 const loginController = require('./controllers/loginController');
 const cadastroController = require('./controllers/cadastroController');
