0e44fef6e7e1bf8a4ed070e1a977750fb64ab932
Eric Pisoni <ericperacchi@gmail.com>
2023-08-23 19:17:13 -0300

    Página aluno.html exibe os dados do aluno.

--- src/views/js/aluno_script.js
+++ src/views/js/aluno_script.js
@@ -1,5 +1,7 @@
-
-
 const usuario = JSON.parse(window.localStorage.getItem('dadosUsuario'));
 
 console.log(usuario);
+
+document.getElementById("dados_cartao").innerHTML = 'Cartão: '+usuario.idusuario
+document.getElementById("dados_nome").innerHTML = 'Nome: '+usuario.nome
+document.getElementById("dados_curso").innerHTML = 'Curso: '+usuario.curso
