c210ddff60981e855222994c944b442beddfbafe
Eric Pisoni <ericperacchi@gmail.com>
2023-08-23 20:07:39 -0300

    Tabela de histórico de aulas

--- src/views/js/historicoDeAulas.js
+++ src/views/js/historicoDeAulas.js
@@ -4,10 +4,24 @@ const tbody = document.querySelector('tbody');
 //const inputTask = document.querySelector('.input-task');
 
 const fetchTasks = async () => {
-  const cartao = usuario.cartao
-  const response = await fetch(`http://localhost:3333/getAulasAluno/${cartao}`)
+  const cartao = usuario.idusuario
+  const response = await fetch(`http://localhost:3333/aulasAluno/${cartao}`)
   const tasks = await response.json()
   return tasks;
+}
+
+const createElement = (tag, innerText = '', innerHTML = '') => {
+  const element = document.createElement(tag);
+
+  if (innerText) {
+    element.innerText = innerText;
+  }
+
+  if (innerHTML) {
+    element.innerHTML = innerHTML;
+  }
+
+  return element;
 }
 
 const createRow = (task) => {
@@ -16,23 +30,23 @@ const createRow = (task) => {
   const { idaula, idaluno, idmonitor, disciplina, status, inicio, fim, modalidade } = task;
 
   const tr = createElement('tr');
-  const td_idaula = createElement('td', idaula);
-  const td_idaluno = createElement('td', idaluno);
-  const td_idmonitor = createElement('td', idmonitor);
+  // const td_idaula = createElement('td', idaula);
+  // const td_idaluno = createElement('td', idaluno);
   const td_disciplina = createElement('td', disciplina);
+  const td_idmonitor = createElement('td', idmonitor);
   const td_status = createElement('td', status);
   const td_inicio = createElement('td', inicio);
-  const td_modalidade = createElement('td', modalidade);
   const td_fim = createElement('td', fim);
+  const td_modalidade = createElement('td', modalidade);
 
-  tr.appendChild(td_idaula);
-  tr.appendChild(td_idaluno);
-  tr.appendChild(td_idmonitor);
+  // tr.appendChild(td_idaula);
+  // tr.appendChild(td_idaluno);
   tr.appendChild(td_disciplina);
+  tr.appendChild(td_idmonitor);
   tr.appendChild(td_status);
   tr.appendChild(td_inicio);
-  tr.appendChild(td_modalidade);
   tr.appendChild(td_fim);
+  tr.appendChild(td_modalidade);
 
   return tr;
 }
