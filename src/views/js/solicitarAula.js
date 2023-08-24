61bfdc07980990fcd9f343eb868b5b16c277d2e9
Eric Pisoni <ericperacchi@gmail.com>
2023-08-23 22:44:49 -0300

    Rota do solicitarAula arrumado Cadastro alinhado

--- src/views/js/solicitarAula.js
+++ src/views/js/solicitarAula.js
@@ -4,57 +4,65 @@ const barra = document.querySelector('p');
 const disciplinas = usuario.disciplinasmatriculadas
 
 const createRow = async (m) => {
+  const response = await fetch(`http://localhost:3333/login/${m}`);
+  const monitor = await response.json();
 
-    const response = await fetch(`http://localhost:3333/login/${m}`);
-    const monitor = await response.json();
+  const { id, nome} = monitor;
 
-    const { id, nome} = monitor;
-  
-    const tr = createElement('tr');
-    const td_id = createElement('td', id);
-    const td_nome = createElement('td', nome);
-  
-    tr.appendChild(td_id);
-    tr.appendChild(td_nome);
+  const tr = createElement('tr');
+  const td_id = createElement('td', id);
+  const td_nome = createElement('td', nome);
 
-    return tr;
-  }
+  tr.appendChild(td_id);
+  tr.appendChild(td_nome);
+
+  return tr;
+}
 
 const createElement = (tag, innerText = '', innerHTML = '') => {
-    const element = document.createElement(tag);
-  
-    if (innerText) {
-      element.innerText = innerText;
-    }
-  
-    if (innerHTML) {
-      element.innerHTML = innerHTML;
-    }
-  
-    return element;
+  const element = document.createElement(tag);
+
+  if (innerText) {
+    element.innerText = innerText;
+  }
+
+  if (innerHTML) {
+    element.innerHTML = innerHTML;
+  }
+
+  return element;
 }
 
 const createSelect = async() => {
+  let options = ""
+  disciplinas.forEach((discp) => {
+      options = options + `<option value="` + discp + `">` + discp + "</option>"
+  });
 
-    const options = ""
-    disciplinas.forEach((discp) => {
-        options = options +"<option value="+ discp +">"+ discp +"</option>"
-      });
+  console.log(options);
 
-    const select = createElement('select', '', options);
-    
-    return select;
+  const select = createElement('select', '', options);
+  
+  return select;
 }
 
-  const fetchMonitores = async (e) => {
-    const monitores = await fetchTasks(`http://localhost:3333/disciplina/${e}`);
+const fetchMonitores = async (e) => {
+  const nomedisc = { nome: e };
   
-    tbody.innerHTML = '';
-  
-    monitores.forEach((monitor) => {
-      const tr = createRow(monitor);
-      tbody.appendChild(tr);
-    });
+  response = await fetch('http://localhost:3333/disciplina', {
+    method: 'get',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(nomedisc)
+  });
+
+  monitores = await response.json();
+
+  tbody.innerHTML = '';
+
+  monitores.forEach((monitor) => {
+    const tr = createRow(monitor);
+    tbody.appendChild(tr);
+  });
 }
 
 barra.appendChild(createSelect());
